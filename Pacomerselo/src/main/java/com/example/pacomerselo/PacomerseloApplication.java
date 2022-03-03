775b7d4939556ee558fe4071c9d3cd2f69384273
yisusrg05 <56789931+yisusrg05@users.noreply.github.com>
2022-03-03 20:06:09 +0100

    Subida del proyecto de IntelliJ

---
+++ Pacomerselo/src/main/java/com/example/pacomerselo/PacomerseloApplication.java
@@ -0,0 +1,13 @@
+package com.example.pacomerselo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacomerseloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacomerseloApplication.class, args);
+    }
+
+}

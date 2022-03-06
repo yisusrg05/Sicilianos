ae0670e67d5c30dc489c9960c20972d671558119
yisusrg05 <56789931+yisusrg05@users.noreply.github.com>
2022-03-06 15:27:39 +0100

    Creado el REST Controller y modificado el Holder del Restaurante

---
+++ Pacomerselo/src/main/java/com/example/pacomerselo/RESTController.java
@@ -0,0 +1,113 @@
+package com.example.pacomerselo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RequestMapping("/api")
+@RestController
+public class RESTController {
+
+    @Autowired
+    RestaurantHolder restaurantHolder;
+    //DishesHolder dishesHolder;
+
+    //////////////////////RESTAURANTS REST CONTROLLER//////////////////////
+    @PostMapping("/restaurant")
+    public ResponseEntity<Restaurant> newRestaurant(@RequestBody Restaurant restaurant){
+        restaurantHolder.addRestaurant(restaurant);
+        return new ResponseEntity<>(restaurant, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/restaurant")
+    public Collection<Restaurant> getRestaurants(){
+        return restaurantHolder.getRestaurants();
+    }
+
+    @GetMapping("/restaurant/{id}")
+    public ResponseEntity<Restaurant> getRestaurants(@PathVariable long id){
+        Restaurant restaurant = restaurantHolder.getRestaurant(id);
+        if(restaurant != null){
+            return new ResponseEntity<>(restaurant,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/restaurant/{id}")
+    public ResponseEntity<Restaurant> updateRestaurant(@PathVariable long id, @RequestBody Restaurant newRestaurant) {
+        Restaurant oldRestaurant = restaurantHolder.getRestaurant(id);
+        if (oldRestaurant!= null) {
+            newRestaurant.setId(id);
+            newRestaurant.setDishes(oldRestaurant.getDishes());
+            restaurantHolder.updateRestaurant(id,newRestaurant);
+            return new ResponseEntity<>(newRestaurant, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/restaurant/{id}")
+    public ResponseEntity<Restaurant> deleteRestaurant(@PathVariable long id) {
+        Restaurant restaurant = restaurantHolder.removeRestaurant(id);
+        if(restaurant != null){
+            return new ResponseEntity<>(restaurant,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //////////////////////DISHES REST CONTROLLER//////////////////////
+    @PostMapping("/restaurant/{id}/dishes")
+    public ResponseEntity<Dishes> newDish(@PathVariable long id,@RequestBody Dishes dish){
+        Restaurant restaurant = restaurantHolder.getRestaurant(id);
+        if (restaurant!= null) {
+            restaurantHolder.addDish(id,dish);
+            return new ResponseEntity<>(dish, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/restaurant/{id}/dishes")
+    public Collection<Dishes> getDishes(@PathVariable long id){
+        return restaurantHolder.getDishes(id);
+    }
+
+    @GetMapping("/restaurant/{id1}/dishes/{id2}")
+    public ResponseEntity<Dishes> getDish(@PathVariable long id1,@PathVariable long id2){
+        Dishes dish = restaurantHolder.getDish(id1,id2);
+        if(dish != null){
+            return new ResponseEntity<>(dish,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/restaurant/{id1}/dishes/{id2}")
+    public ResponseEntity<Dishes> updateDish(@PathVariable long id1, @PathVariable long id2, @RequestBody Dishes newDish) {
+        Dishes oldDish=restaurantHolder.getDish(id1,id2);
+        if (oldDish!= null) {
+            newDish.setId(id2);
+            restaurantHolder.updateDish(id1,id2,newDish);
+            return new ResponseEntity<>(newDish, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/restaurant/{id1}/dishes/{id2}")
+    public ResponseEntity<Dishes> deleteDish(@PathVariable long id1,@PathVariable long id2) {
+        Dishes dish= restaurantHolder.removeDish(id1,id2);
+        if(dish != null){
+            return new ResponseEntity<>(dish,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
--- Pacomerselo/src/main/java/com/example/pacomerselo/Restaurant.java
+++ Pacomerselo/src/main/java/com/example/pacomerselo/Restaurant.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Data
@@ -17,19 +17,27 @@ public class Restaurant {
 
     private String name;
     private String description;
-    private List<Dishes> dishesList;
     private long id=-1;
-    private AtomicLong lastID= new AtomicLong();
+    private Map<Long,Dishes> dishes= new ConcurrentHashMap<>();
 
     public Restaurant(String name,String description){
         this.name=name;
         this.description=description;
-        this.dishesList= new ArrayList<>();
     }
 
-    public void add(Dishes dish){
-        long idDish = lastID.incrementAndGet();
-        dish.setId(idDish);
-        this.dishesList.add(dish);
+    public Dishes add(long id, Dishes dish){
+        return this.dishes.put(id,dish);
+    }
+
+    public Collection<Dishes> allDishes (){
+        return this.dishes.values();
+    }
+
+    public Dishes getDish (long id){
+        return this.dishes.get(id);
+    }
+
+    public Dishes removeDish(long id){
+        return this.dishes.remove(id);
     }
 }

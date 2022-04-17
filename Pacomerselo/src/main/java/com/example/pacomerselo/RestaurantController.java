956e12c51dad63db2c625cd6712904bae7d962d8
siciliagc <99964708+siciliagc@users.noreply.github.com>
2022-04-17 18:52:57 +0200

    El usuario que está intentando registrar ya existe

--- Pacomerselo/src/main/java/com/example/pacomerselo/RestaurantController.java
+++ Pacomerselo/src/main/java/com/example/pacomerselo/RestaurantController.java
@@ -155,6 +155,10 @@ public class RestaurantController {
     public String incorrectEmailOPassword(){
         return "incorrectEmailoPassword";
     }
+    @GetMapping("/alredyExistingUser")
+    public String alredyExistingUser(){
+        return "alredyExistingUser";
+    }
 
     @GetMapping ("/registerRestaurant")
     public String registerRestaurant(){

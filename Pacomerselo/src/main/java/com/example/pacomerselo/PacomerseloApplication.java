54838baf290c8b5c752b02bc85cc73112036348b
siciliagc <99964708+siciliagc@users.noreply.github.com>
2022-04-26 20:13:35 +0200

    Añadidos restaurantes (faltan las fotos)
    Las fotos las añado luego <3

--- Pacomerselo/src/main/java/com/example/pacomerselo/PacomerseloApplication.java
+++ Pacomerselo/src/main/java/com/example/pacomerselo/PacomerseloApplication.java
@@ -25,6 +25,10 @@ public class PacomerseloApplication {
             Restaurant laBonita = new Restaurant("La Bonita", "Comida Original", "Mediterránea");
             Restaurant fellina = new Restaurant("Fellina", "Comida Italiana Sofisticada", "Italiana");
             Restaurant burmet = new Restaurant("Burmet", "Auténtica Comida Americana en Madrid", "Americana");
+            Restaurant zielou = new Restaurant("Zielou", "Un nuevo concepto gastronómico llevado al límite.", "Internacional");
+            Restaurant tagoMago = new Restaurant("Tagomago", "En sus platos encontrarás una fusión de sabores", "Internacional");
+            Restaurant bizzo = new Restaurant("Bizzo", "La pizza más canalla de Madrid", "Pizza");
+
 
             restaurantRepository.save(laBonita);
 
@@ -51,6 +55,28 @@ public class PacomerseloApplication {
             Dishes dish33 = new Dishes("Vigi Roll", "Vegano. Calabaza, cebolla y espinacas", 13, DishType.MAIN.label);
             Dishes dish34 = new Dishes("Carrot Cake", "Buttercream de chocolate blanco y almendras", 7, DishType.DESSERT.label);
             Dishes dish35 = new Dishes("Cheese Cake", "Casera al estilo Nueva York", 7, DishType.DESSERT.label);
+
+            Dishes dish42=new Dishes("Ceviche de Corvina", "Con leche de tigre de ají-limo, cancha tostada, plátano macho y crujiente de patata violeta.", 22, DishType.STARTER.label);
+            Dishes dish43=new Dishes("Tartar de Atún rojo de Balfegó", "Sashimi al natural acompañado de salsa roja especiada Tartar sobre pan brioche aliñado con kimchee coreano y aceite de soja.", 29, DishType.STARTER.label);
+            Dishes dish44=new Dishes("Trufón de rabo de toro", "Sobre parmentier de patata trufada y foie", 28, DishType.MAIN.label);
+            Dishes dish45=new Dishes("Udón Vegano al curry Massaman", "Con curry Massaman, setas shiitake, pak choy, tofu y cebolleta china", 22, DishType.MAIN.label);
+            Dishes dish46=new Dishes("Tarta de queso Payoyo (2 pax)", "Con crujiente de regaliz, fina galleta de canela y manzana caramelizada.", 20, DishType.DESSERT.label);
+            Dishes dish47=new Dishes("Tarta de chocolate", "70% de cacao con núcleo de ganache ligero de chocolate y naranja, acompañadas de grageas de colores.", 29, DishType.STARTER.label);
+
+            Dishes dish60=new Dishes("Pulpo sobre alioli de patata", "Con aceite de pimentón y crujiente de jamón.", 10, DishType.STARTER.label);
+            Dishes dish61=new Dishes("Berenjena caramelizada", "Con shitake, crema agria y romesco.", 12, DishType.STARTER.label);
+            Dishes dish62=new Dishes("Lentejas melosas de marisco", "Con alioli y ajo tierno.", 17, DishType.MAIN.label);
+            Dishes dish63=new Dishes("Costillar ibérico", "Cocinado a baja temperatura.", 25, DishType.MAIN.label);
+            Dishes dish64=new Dishes("Tarta banofee", "Galleta, plátano, dulce de leche y nata.", 9, DishType.DESSERT.label);
+            Dishes dish65=new Dishes("Tarta cremosa de lima", "Con base de galleta y nata .", 29, DishType.DESSERT.label);
+
+            Dishes dish80=new Dishes("Bollito", "Barrita de pan al horno acompañada de nuestra salsa especial con un toque de ajo y orégano.", 5, DishType.STARTER.label);
+            Dishes dish81=new Dishes("The Provolone", "Pomodoro Bizzo, provolone dulce y  un toque de orégano. Acompañado de nuestro pan de pizza artesano.", 29, DishType.STARTER.label);
+            Dishes dish82=new Dishes("Pizza Curcubita", "Pomodoro Bizzo, mozzarella, calabacín marinado y un toque de parmesano.", 13, DishType.MAIN.label);
+            Dishes dish83=new Dishes("Pizza Mucca", "Pomodoro Bizzo, mozzarella, costilla de ternera deshuesada aderezada con un mix de especias y queso cheddar.", 15, DishType.MAIN.label);
+            Dishes dish84=new Dishes("Pizza Pitio", "Base de dulce de leche aderezada con queso gorgonzola.", 10, DishType.DESSERT.label);
+            Dishes dish85=new Dishes("Pizza Celia", "Base de nutella con topping de oreo y lacasitos.", 10, DishType.DESSERT.label);
+
 
             laBonita.add(dish11);
             laBonita.add(dish12);
@@ -123,6 +149,73 @@ public class PacomerseloApplication {
             dishesRepository.save(dish33);
             dishesRepository.save(dish34);
             dishesRepository.save(dish35);
+
+            restaurantRepository.save(zielou);
+            zielou.add(dish42);
+            zielou.add(dish43);
+            zielou.add(dish44);
+            zielou.add(dish46);
+            zielou.add(dish47);
+
+            dish42.setRestaurant(zielou);
+            dish43.setRestaurant(zielou);
+            dish44.setRestaurant(zielou);
+            dish45.setRestaurant(zielou);
+            dish46.setRestaurant(zielou);
+            dish47.setRestaurant(zielou);
+
+            dishesRepository.save(dish42);
+            dishesRepository.save(dish43);
+            dishesRepository.save(dish44);
+            dishesRepository.save(dish45);
+            dishesRepository.save(dish46);
+            dishesRepository.save(dish47);
+
+            restaurantRepository.save(tagoMago);
+            tagoMago.add(dish60);
+            tagoMago.add(dish61);
+            tagoMago.add(dish62);
+            tagoMago.add(dish63);
+            tagoMago.add(dish64);
+            tagoMago.add(dish65);
+
+            dish60.setRestaurant(tagoMago);
+            dish61.setRestaurant(tagoMago);
+            dish62.setRestaurant(tagoMago);
+            dish63.setRestaurant(tagoMago);
+            dish64.setRestaurant(tagoMago);
+            dish65.setRestaurant(tagoMago);
+
+            dishesRepository.save(dish60);
+            dishesRepository.save(dish61);
+            dishesRepository.save(dish62);
+            dishesRepository.save(dish63);
+            dishesRepository.save(dish64);
+            dishesRepository.save(dish65);
+
+            restaurantRepository.save(bizzo);
+
+            bizzo.add(dish80);
+            bizzo.add(dish81);
+            bizzo.add(dish82);
+            bizzo.add(dish83);
+            bizzo.add(dish84);
+            bizzo.add(dish85);
+
+            dish80.setRestaurant(bizzo);
+            dish81.setRestaurant(bizzo);
+            dish82.setRestaurant(bizzo);
+            dish83.setRestaurant(bizzo);
+            dish84.setRestaurant(bizzo);
+            dish85.setRestaurant(bizzo);
+
+            dishesRepository.save(dish80);
+            dishesRepository.save(dish81);
+            dishesRepository.save(dish82);
+            dishesRepository.save(dish83);
+            dishesRepository.save(dish84);
+            dishesRepository.save(dish85);
+
 
 
             User paco = new User("Anonimo", "Paco", "Mérselo", "paco@pacomerselo.es", "oleole", true);

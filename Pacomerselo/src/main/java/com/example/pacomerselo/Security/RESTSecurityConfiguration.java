44be97d613fe90d107049a97170b3140af57b15a
yisusrg05 <56789931+yisusrg05@users.noreply.github.com>
2022-05-22 02:18:41 +0200

    oleole

---
+++ Pacomerselo/src/main/java/com/example/pacomerselo/Security/RESTSecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.pacomerselo.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+public class RESTSecurityConfiguration {
+}
